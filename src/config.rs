c715af2c037c2bf4b845e896fc4aadae1bfa8bd5
Jonas <me@jbrn.eu>
2020-03-19 21:12:27 +0100

    Add app config

---
+++ src/config.rs
@@ -0,0 +1,30 @@
+use config::{Config, ConfigError, Environment, File};
+use serde::Deserialize;
+use std::env;
+use std::path::PathBuf;
+
+const DEFAULT_CONFIG_PATH: &str = "config.toml";
+
+#[derive(Debug, Deserialize)]
+pub struct YaatsConfig {
+    pub listen_address: String,
+    pub redis_url: String,
+    pub authorization_url: Option<String>,
+}
+
+impl YaatsConfig {
+    pub fn new() -> Result<Self, ConfigError> {
+        let mut cfg = Config::new();
+
+        let config_path =
+            env::var("YAATS_CONFIG").unwrap_or_else(|_| String::from(DEFAULT_CONFIG_PATH));
+
+        if PathBuf::from(&config_path).exists() {
+            cfg.merge(File::with_name(&config_path))?;
+        }
+
+        cfg.merge(Environment::with_prefix("YAATS"))?;
+
+        cfg.try_into()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use crate::config::YaatsConfig;
+use actix_web::{App, HttpServer};
+
+mod config;
+
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config =
+        YaatsConfig::new().map_err(|err| format!("Error reading configuration: {}", err))?;
+
+    HttpServer::new(move || App::new())
+        .bind(config.listen_address)?
+        .run()
+        .await?;
+
     Ok(())
 }
